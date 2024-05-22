54eb3f1eeb297ba8bd8e63666c73178248688bfc
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-23 01:40:38 +0300

    added database structure and user app

---
+++ station/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+from train_station_api_service import settings
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(TrainType, on_delete=models.CASCADE, related_name='trains')
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='routes')
+    destination = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='routes')
+    distance = models.IntegerField()
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='journies')
+    train = models.ForeignKey(Train, on_delete=models.CASCADE, related_name='journies')
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(Journey, on_delete=models.CASCADE, related_name='tickets')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='tickets')

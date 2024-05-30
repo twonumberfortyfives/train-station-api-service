9833ddbbe95d5faea1a8d85210212bb2f1789b81
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-30 23:12:58 +0300

    code refactoring

--- train_station_api_service/settings.py
+++ train_station_api_service/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 
-# Add .env variables anywhere before SECRET_KEY
+# Add env.sample variables anywhere before SECRET_KEY
 load_dotenv(find_dotenv())
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
